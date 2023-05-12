eff822cfaa899bcf671ceb637d2cecfdbbe1c716
Filiberto <filiberto.dev@gmail.com>
2023-05-12 02:48:07 +0200

    feat: init flask_sqlalchemy database

--- website/__init__.py
+++ website/__init__.py
@@ -1,10 +1,18 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
 
 
 def create_app():
     app = Flask(__name__)  # name of the file
 
     app.config['SECRET_KEY'] = 'notFil6e660'  # use .env file in production
+    # TODO change sqlite with postgressql (look up docs)
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+
+    db.init_app(app)  # init db
 
     # blueprints
     from .views import views

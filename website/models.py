42fe36bb94dcce6beb5f3c0893e1a21d3c29410d
Filiberto <filiberto.dev@gmail.com>
2023-05-12 18:07:32 +0200

    chore: db models

--- website/models.py
+++ website/models.py
@@ -1,1 +1,15 @@
-# going to use it as a sort of database models
+from . import db
+from flask_login import UserMixin
+from sqlalchemy.sql import func
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
+
+
+class Note():
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(10000))
+    date = db.Column(db.DateTime(timezone=True), default=func.now())

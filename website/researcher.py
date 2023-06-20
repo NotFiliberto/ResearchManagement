885c39e5513c27215d2353f662ef1cbd0261deed
Filiberto <filiberto.dev@gmail.com>
2023-06-20 15:30:32 +0200

    chore: create project route

--- website/researcher.py
+++ website/researcher.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash, redirect, url_for
 from flask_login import login_required, current_user
 
 researcher = Blueprint('researcher', __name__)
@@ -9,3 +9,11 @@ researcher = Blueprint('researcher', __name__)
 @login_required
 def researcher_home():
     return render_template('researcher/home.html', user=current_user)
+
+
+# create project page
+@researcher.route('/create', methods=['GET', 'POST'])
+@login_required
+def create_project():
+    if request.method == "GET":
+        return render_template('researcher/create.html', user=current_user)

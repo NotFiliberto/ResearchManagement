1f7d694d95022e387a1fd1df5dc1fe41db4600ac
VineetCesca <97135027+VineetCesca@users.noreply.github.com>
2023-06-21 01:15:35 +0200

    pre pull dev

--- website/researcher.py
+++ website/researcher.py
@@ -32,6 +32,14 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+def researcher_only(route_function):
+    def decorated_function(*args, **kwargs):
+        # Verifica se l'utente corrente è un ricercatore
+        if not current_user.__class__.__name__ == "Researcher":
+            return redirect(url_for('researcher.researcher_home'))  # Reindirizza all'home dei ricercatori
+        return route_function(*args, **kwargs)
+    return decorated_function
+
 
 researcher = Blueprint('researcher', __name__)
 
@@ -52,14 +60,26 @@ def researcher_home():
 
 @researcher.route('/create', methods=['GET', 'POST'])
 @login_required
+@researcher_only
 def create_project():
 
     if request.method == "GET":
         return render_template('researcher/create.html', user=current_user)
 
     if (request.method == "POST"):
+
+        # Salvo i dati per poi creare un Progetto nel DB
+        project_name = request.form.get('project_name') 
+        description = request.form.get('description')
+        print("project info -> name: " + project_name + " description: " + description + " researcher_id: " + str(current_user.id))
+
+        # Creating DB Project
+        project = Project(name=project_name, researcher_id=current_user.id)
+        db.session.add(project)
+        db.session.commit()
+
         # Get the list of files from webpage
-        print('UPLOAD???')
+        # print('UPLOAD???')
         files = request.files.getlist("file")
 
         # Iterate for each file in the files List, and Save them
@@ -67,4 +87,15 @@ def create_project():
         for file in files:
             print("file --> ", file.filename, i)
             i += 1
-        return "ok bro"
+
+        print("filename ", file.filename, " , length filename ", len(file.filename))
+        if len(file.filename) == 0:
+            print('no files loaded')
+            flash('No file was loaded', category='error')
+        else:
+            print('Files loaded correctly')
+            flash('Files loaded', category='success')
+
+            # Se sono stati caricati dei file
+
+    return render_template('researcher/create.html', user=current_user)

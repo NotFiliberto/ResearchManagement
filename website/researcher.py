ce9b07b508d78279b88653e7487e12673eedc439
VineetCesca <97135027+VineetCesca@users.noreply.github.com>
2023-06-21 03:55:14 +0200

    pre db test

--- website/researcher.py
+++ website/researcher.py
@@ -3,28 +3,21 @@ import shutil
 from flask import Blueprint, Flask, app, render_template, request, flash
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 from flask_login import login_required, current_user
-from .models import Project, ProjectStatus, Evaluation_Interval, Researcher
+from .models import Project, ProjectStatus, Evaluation_Interval, Researcher, Document
 from werkzeug.utils import secure_filename
 from . import db
 import datetime
 
-# creare una cartelle col nome del progetto
-def crea_cartella(nome_cartella):
-    try:
-        os.mkdir(nome_cartella)
-        print(f"Folder '{nome_cartella}' has been succesfully created")
-    except FileExistsError:
-        print(f"Folder '{nome_cartella}' already exists.")
+# creare una cartella specificando dove e con che nome
+def crea_cartella(cartella_padre, nome_nuova_cartella):
+    percorso_nuova_cartella = os.path.join(cartella_padre, nome_nuova_cartella)
+    os.mkdir(percorso_nuova_cartella)
+
 
 # prendere un file e metterlo dentro una cartella specificando il nome
-def sposta_file_in_cartella(file_da_spostare, cartella_destinazione):
-    try:
-        shutil.move(file_da_spostare, cartella_destinazione)
-        print(f"Il file '{file_da_spostare}' è stato spostato nella cartella '{cartella_destinazione}' con successo.")
-    except FileNotFoundError:
-        print(f"Il file '{file_da_spostare}' non esiste.")
-    except shutil.Error:
-        print(f"Si è verificato un errore nello spostamento del file '{file_da_spostare}'.")
+def move_file_into(file_da_spostare, cartella_destinazione):
+    shutil.move(file_da_spostare, cartella_destinazione)
+    print(f"Il file '{file_da_spostare}' è stato spostato nella cartella '{cartella_destinazione}' con successo.")
 
 ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 # allowed files
@@ -68,34 +61,63 @@ def create_project():
 
     if (request.method == "POST"):
 
-        # Salvo i dati per poi creare un Progetto nel DB
+        # Saving data request form into DB Project 
         project_name = request.form.get('project_name') 
         description = request.form.get('description')
         print("project info -> name: " + project_name + " description: " + description + " researcher_id: " + str(current_user.id))
 
-        # Creating DB Project
+        # Creating DB Project (no description -> add it to the document topic? OR add id to Project model?)
         project = Project(name=project_name, researcher_id=current_user.id)
-        db.session.add(project)
-        db.session.commit()
+        #db.session.add(project)
+        #db.session.commit()
+
 
         # Get the list of files from webpage
         # print('UPLOAD???')
         files = request.files.getlist("file")
+        print(files)
 
-        # Iterate for each file in the files List, and Save them
+        # Iterate for each file in the files List, and Save them (non li scorre tutti bro, solo 1 bro)
         i = 0
         for file in files:
-            print("file --> ", file.filename, i)
-            i += 1
+            print("file ", i, " -->  filename ", file.filename, " , length filename ", len(file.filename))
+            # Wether a file si valid and was uploaded inside the form (files)
+            if len(file.filename) == 0:
+                print('no files loaded')
+                flash('No file was loaded', category='error')
+            else:
+                extension = os.path.splitext(file.filename)[1]
+                if extension.lower() != '.pdf':
+                    print("ONLY PDFS")
+                    flash('Upload only PDFs', category='error')
+                    return redirect(url_for('researcher.researcher_home'))
+                document = Document(file_extension=extension, file_name=file.filename, 
+                                    topic=description, project_id=project.project_id)
+                project.project_id = 2
+                print("extension: ", extension, " project_id: ", project.project_id)
+                #db.session.add(document)
+                #db.session.commit()
 
-        print("filename ", file.filename, " , length filename ", len(file.filename))
-        if len(file.filename) == 0:
-            print('no files loaded')
-            flash('No file was loaded', category='error')
-        else:
-            print('Files loaded correctly')
-            flash('Files loaded', category='success')
+                filename = secure_filename(file.filename)
 
-            # Se sono stati caricati dei file
+                folder_save_into = 'project_files'
+                sub_folder = str(project.project_id)
+
+                path_save_into = os.path.join(folder_save_into, sub_folder)
+                if not os.path.exists(path_save_into):
+                    os.makedirs(path_save_into)
+
+                path_to_file = os.path.join(path_save_into, filename)
+                if os.path.exists(path_to_file):
+                    print("File already exists in the destination folder")
+                    flash('File already exists', category='error')
+                    return redirect(url_for('researcher.researcher_home'))
+
+                file.save(os.path.join(path_save_into, filename))
+                
+                print('Files loaded correctly')
+                flash('Files loaded', category='success')
+                i += 1
+    
 
     return render_template('researcher/create.html', user=current_user)

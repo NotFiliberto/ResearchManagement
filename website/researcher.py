1109ac6f15538b2adff50f389e7617ea026ad2d3
Filiberto <filiberto.dev@gmail.com>
2023-06-22 16:09:19 +0200

    chore: flash messages in create and home pages

--- website/researcher.py
+++ website/researcher.py
@@ -12,30 +12,33 @@ import datetime
 def read_project(project_id):
     p = Project.query.filter_by(project_id=project_id).first()
     # making a list of the project info
-    project_info = [ p.project_id, p.name, p.status, p.researcher_id, p.evaluation_interval_id]
+    project_info = [p.project_id, p.name, p.status,
+                    p.researcher_id, p.evaluation_interval_id]
     interval = Evaluation_Interval.query.filter_by(
-    evaluation_interval_id=p.evaluation_interval_id).first()
+        evaluation_interval_id=p.evaluation_interval_id).first()
     researcher = Researcher.query.filter_by(id=p.researcher_id).first()
     documents = Document.query.filter_by(project_id=p.project_id)
-    project_info.append(documents.count()) 
+    project_info.append(documents.count())
     for d in documents:
         project_info.append(d)
 
-    project_div = [project_info, [interval.start, interval.end], [researcher.username, researcher.email]]
+    project_div = [project_info, [interval.start, interval.end],
+                   [researcher.username, researcher.email]]
     return project_div
 
 
 def restrict_user(current_usr, user_type):
     def decorator(route_function):
         def decorated_function(*args, **kwargs):
-        # Check if user is a Researcher
+            # Check if user is a Researcher
             if not current_usr.__class__.__name__ == str(user_type):
-                flash('You need to be a ' + str(user_type) + ' user to access that page', category='error')
+                flash('You need to be a ' + str(user_type) +
+                      ' user to access that page', category='error')
                 return redirect(url_for('auth.sign_in'))
             return route_function(*args, **kwargs)
         return decorated_function
     return decorator
-    
+
 
 researcher = Blueprint('researcher', __name__)
 
@@ -61,32 +64,33 @@ def create_project():
         return render_template('researcher/create.html', user=current_user)
 
     if request.method == "POST":
-        # Saving data request form into DB Project 
-        project_name = request.form.get('project_name') 
+        # Saving data request form into DB Project
+        project_name = request.form.get('project_name')
         description = request.form.get('description')
 
         # Creating DB Project (ATTENTION EV_INTERVAL=1 TEMPORARY VALUE TESTING)
         project = Project(name=project_name, evaluation_interval_id=1,
-                           researcher_id=current_user.id)
+                          researcher_id=current_user.id)
         db.session.add(project)
         db.session.commit()
 
         files = request.files.getlist("file")
 
-        # Iterate each file in the files List and save them 
+        # Iterate each file in the files List and save them
         i = 0
         for file in files:
             # Wether a file si valid and was uploaded inside the form (files)
             if len(file.filename) == 0:
+                # flash error on /researcher/create.html
                 flash('No file was loaded', category='error')
+
             else:
                 extension = os.path.splitext(file.filename)[1]
                 if extension.lower() != '.pdf':
                     flash('Upload only PDFs', category='error')
-                    return redirect(url_for('researcher.researcher_home'))
-                
+
                 filename = secure_filename(file.filename)
-                
+
                 folder_save_into = 'project_files'
                 sub_folder = str(project.project_id)
 
@@ -97,19 +101,16 @@ def create_project():
                 path_to_file = os.path.join(path_save_into, filename)
                 if os.path.exists(path_to_file):
                     flash('File already exists', category='error')
-                    return redirect(url_for('researcher.researcher_home'))
                 # Create a document only if file does not already exists
-                document = Document(file_extension=extension, file_name=file.filename, 
+                document = Document(file_extension=extension, file_name=file.filename,
                                     topic=description, project_id=project.project_id)
-                #ADD Document into DB 
+                # ADD Document into DB
                 db.session.add(document)
                 db.session.commit()
 
                 file.save(os.path.join(path_save_into, filename))
-                
+
                 flash('Files loaded', category='success')
                 i += 1
-    
+
     return render_template('researcher/create.html', user=current_user)
-
-

b0b2635b52c5a5da7a541e3323a0c6fbc3506492
VineetCesca <97135027+VineetCesca@users.noreply.github.com>
2023-06-24 00:15:40 +0200

    chore: define get_project function minimal

--- website/utils.py
+++ website/utils.py
@@ -1,5 +1,12 @@
-from flask import Blueprint, render_template, flash
-from flask_login import login_required, current_user
+from .researcher import get_Researcher_Project
+from .evaluator import get_Evaluator_Project
 
-utils = Blueprint('utils', __name__)
+
+
+def get_project(current_user, project_id):
+    if current_user.__class__.__name__ == "Researcher":
+        return get_Researcher_Project(project_id)
+    elif current_user.__class__.__name__ == "Evaluator":
+        return get_Evaluator_Project(project_id)
+
 

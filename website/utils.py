b346dc7f161dc834da62fe1755a5d01db30511c1
VineetCesca <97135027+VineetCesca@users.noreply.github.com>
2023-06-24 04:03:01 +0200

    chore: get_project() without any class

--- website/utils.py
+++ website/utils.py
@@ -1,66 +1,35 @@
 from .models import Project, Researcher, Document, ProjectStatus, Evaluation_Interval
 from flask import redirect, flash, url_for
 from functools import wraps
+from collections import namedtuple
 
-
-class CustomProject1:
-    def __init__(self, project_id, name, description, researcher, documents):
-        self.id = project_id
-        self.name = name
-        self.description = description
-        self.researcher = researcher
-        self.documents = documents
-
-
-class CustomProject2:
-    def __init__(self, project_id, name, status, description, researcher, documents):
-        self.id = project_id
-        self.name = name
-        self.status = status
-        self.description = description
-        self.researcher = researcher
-        self.documents = documents
-
-
-class CustomResearcher:
-            def __init__(self, r_id, name, username):
-                self.id = r_id
-                self.name = name
-                self.username = username
-
-
-class CustomDocuments:
-            def __init__(self, d_id, name):
-                self.id = d_id
-                self.name = name
-
-
-def r_definition(p, researcher, documents):
-    return CustomProject1( project_id=p.project_id, name=p.name, description=p.description, 
-                            researcher=researcher,
-                            documents=documents)
-
-
-def e_definition(p, researcher, documents):
-    return CustomProject2( project_id=p.project_id, name=p.name, 
-                          status=ProjectStatus.SUBMITTED_FOR_EVALUATION, description=p.description, 
-                            researcher=researcher,
-                            documents=documents)
 
 
 def get_project(project_id):
     p = Project.query.filter_by(project_id=project_id).first()
     r = Researcher.query.filter_by(id=p.researcher_id).first()
     docs = Document.query.filter_by(project_id=project_id)
-    researcher = CustomResearcher(r_id=r.id, name=r.email, username=r.username)
-    documents = []
-    
-    for d in docs:
-        documents.append( CustomDocuments(d_id=d.document_id, name=d.file_name) )
 
-    return CustomProject1( project_id=p.project_id, name=p.name, description=p.description, 
-                            researcher=researcher,
-                            documents=documents)
+    P = namedtuple('P', ['id', 'name', 'description', 'status', 'researcher', 'documents'])
+    R = namedtuple('R', ['id', 'name', 'username'])
+    D = namedtuple('D', ['id', 'name'])
+    
+    res = R(id=r.id, name=r.email, username=r.username)
+    
+    documents = []
+    for d in docs:
+        dd = D(id=d.document_id, name=d.file_name)
+        documents.append(dd)
+    
+    project = P(id=p.project_id, 
+                name=p.name, 
+                description=p.description, 
+                status=ProjectStatus.SUBMITTED_FOR_EVALUATION,
+                researcher=res,
+                documents=documents
+                )
+
+    return project
 
 
 def restrict_user(current_user, user_type):
@@ -75,39 +44,4 @@ def restrict_user(current_user, user_type):
             return route_function(*args, **kwargs)
         return decorated_function
     return decorator
-
-
-def print_projects(s, e):
-    projects = Project.query.filter(Project.project_id.between(s, e)).all()
-    list_p = []
-    for p in projects:
-        i = Evaluation_Interval.query.filter_by(evaluation_interval_id=p.evaluation_interval_id).first()
-        r = Researcher.query.filter_by(id=p.researcher_id).first()
-        list_p.append([ p.project_id, p.name, p.description, p.status, 
-                        [i.evaluation_interval_id, i.start, i.end], 
-                        [r.id, r.email, r.username] ])
-    print("Projects here: ")
-    for elem in list_p:
-        print(elem)
-    return list_p
-
-
-def read_project(project_id):
-    p = Project.query.filter_by(project_id=project_id).first()
-    # making a list of the project info
-    print("read: ", p, " id: ", project_id)
-    project_info = [p.project_id, p.name, p.status,
-                    p.researcher_id, p.evaluation_interval_id]
-    interval = Evaluation_Interval.query.filter_by(
-        evaluation_interval_id=p.evaluation_interval_id).first()
-    researcher = Researcher.query.filter_by(id=p.researcher_id).first()
-    documents = Document.query.filter_by(project_id=p.project_id)
-    project_info.append(documents.count())
-    for d in documents:
-        project_info.append(d)
-
-    project_div = [project_info, [interval.start, interval.end],
-                   [researcher.username, researcher.email]]
-    return project_div
-
 

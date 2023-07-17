52c13bf4f268383e2d6b26c911749f000457f6cf
VineetCesca <97135027+VineetCesca@users.noreply.github.com>
2023-07-18 01:05:20 +0200

    refactor: :recycle: download zip filename changes
    distinguish wether project has no name or set project_name as the filename of zip file

--- website/document.py
+++ website/document.py
@@ -2,7 +2,7 @@ from flask import Blueprint, flash, redirect, render_template, request, send_fil
 from flask_login import login_required, current_user
 from website.models import Document
 from website.models import Project
-from .utils import download_document, download_zip_documents, restrict_user
+from .utils import download_document, download_zip_documents, restrict_user, get_project
 
 
 document = Blueprint('document', __name__)
@@ -21,9 +21,13 @@ def download():
 @restrict_user(current_user, ['Researcher', 'Evaluator'])
 def download_zip():
     project_id = request.args.get('project_id')
+    p = get_project(project_id)
     project = Project.query.filter_by(project_id=project_id).first()
     if project_id is None or project is None:
         return redirect(url_for('static', filename='404.html'))
     zip_buffer = download_zip_documents(project_id)
-    name = 'project_{}_files.zip'.format(project_id)
+    if p.name is not None:
+        name = 'project_{}_files.zip'.format(p.name)
+    else:
+        name = 'project_files.zip'
     return send_file(zip_buffer, as_attachment=True, download_name=name)
